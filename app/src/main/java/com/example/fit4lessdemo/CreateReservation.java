61355aba58a6d1683c7eb3e40e00c7e0482732ef
julia <julia.chien@ryerson.ca>
2020-11-11 17:47:12 -0500

    added to the reservation part

--- app/src/main/java/com/example/fit4lessdemo/CreateReservation.java
+++ app/src/main/java/com/example/fit4lessdemo/CreateReservation.java
@@ -26,7 +26,6 @@ public class CreateReservation extends AppCompatActivity {
         setContentView(R.layout.activity_create_reservation);
         onDisplayDate = (TextView) findViewById(R.id.makeReservation);
 
-
         onDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
