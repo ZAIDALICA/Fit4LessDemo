d150c287e3e3c3947fa12e8721d071055f3cacb7
MCampeau95 <63267773+MCampeau95@users.noreply.github.com>
2020-11-14 19:47:51 -0500

    Got the Nav View functions working, I updated my Gradle as well so that changed, I added a small static method to Main to carry name over to other classes for logout, I added Nav view to the map, it works as well. Current page on Nav view should not allow one to re-run the startActivity for that page

--- app/src/main/java/com/example/fit4lessdemo/Map.java
+++ app/src/main/java/com/example/fit4lessdemo/Map.java
@@ -1,22 +1,39 @@
 package com.example.fit4lessdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Map extends AppCompatActivity {
     private String loc;
+    BottomNavigationView bNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Button confirm = findViewById(R.id.confirm_btn);
+        bNav = findViewById(R.id.nav_view);
+        bNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.navigation_home:
+                        startActivity(new Intent(Map.this, NewsPage.class));
+                        return true;
+                    case R.id.navigation_logout:
+                        logMeOutVoid();
+                }
+                return false;
+            }
+        });
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,5 +52,14 @@ public class Map extends AppCompatActivity {
             //For now we will say:
             Toast.makeText(getApplicationContext(), "You have succesfully chosen: " + loc, Toast.LENGTH_LONG).show();
         }
+    }
+    public void logMeOutVoid() {
+        //go to the login page again
+        Intent i = new Intent(Map.this, SignIn.class);
+        String byUsername = MainActivity.getSavedName();
+        Toast.makeText(getApplicationContext(), "Bye "+byUsername, Toast.LENGTH_SHORT).show();
+        //clearing the user preferences
+        SaveUserLoginPreferences.clearUserLoginSharedPreferences(Map.this);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/fit4lessdemo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/fit4lessdemo/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.fit4lessdemo.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.fit4lessdemo.NewsPage;
 import com.example.fit4lessdemo.R;
 
 public class HomeFragment extends Fragment {
