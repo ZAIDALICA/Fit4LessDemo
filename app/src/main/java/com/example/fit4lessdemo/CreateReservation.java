1f204e278163c45852b1d1125808d4eaf8e4556c
julia <julia.chien@ryerson.ca>
2020-11-18 17:16:25 -0500

    added a new file NavigationTester.java, the navigation is in this file

--- app/src/main/java/com/example/fit4lessdemo/CreateReservation.java
+++ app/src/main/java/com/example/fit4lessdemo/CreateReservation.java
@@ -1,6 +1,10 @@
 package com.example.fit4lessdemo;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
@@ -13,6 +17,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+
 import java.util.Calendar;
 
 public class CreateReservation extends AppCompatActivity {
@@ -20,11 +25,19 @@ public class CreateReservation extends AppCompatActivity {
 
     private TextView onDisplayDate;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_reservation);
         //TODO I stopped it for now George onDisplayDate = (TextView) findViewById(R.id.makeReservation);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         onDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +51,7 @@ public class CreateReservation extends AppCompatActivity {
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));     //This makes the background of the selection transparent
                 dialog.show();
             }
+
         });
 
         onDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -45,16 +59,17 @@ public class CreateReservation extends AppCompatActivity {
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month +1 ;
                 Log.d(TAG, "onDateSet: mm/dd/yy:" +year+ "/" +month+ "/" +dayOfMonth);
-
                 String date = month + "/" +dayOfMonth+ "/" +year;
                 onDisplayDate.setText(date);
             }
         };
-
-
-
-
-
-
+    }
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        { super.onBackPressed(); }
     }
 }
---
+++ app/src/main/java/com/example/fit4lessdemo/NavigationTester.java
@@ -0,0 +1,88 @@
+package com.example.fit4lessdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class NavigationTester extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_tester);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById((R.id.nav_view));
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+                case R.id.workout:
+                    Intent workout = new Intent( NavigationTester.this, WorkoutHelp.class);
+                    startActivity(workout);
+                    break;
+                case R.id.reservation:
+                    //bookings();
+                    Intent reservation = new Intent( NavigationTester.this, MyBookings.class);
+                    startActivity(reservation);
+                    break;
+                case R.id.map:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapsFragment()).commit();
+                    break;
+                case R.id.nav_logout:
+                    logMeOutVoid();
+                    break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        { super.onBackPressed(); }
+    }
+
+
+    public void logMeOutVoid() {
+        //go to the login page again
+        Intent i = new Intent(NavigationTester.this, SignIn.class);
+        String byUsername = MainActivity.getSavedName();
+        Toast.makeText(getApplicationContext(), "Bye "+byUsername, Toast.LENGTH_SHORT).show();
+        //clearing the user preferences
+        SaveUserLoginPreferences.clearUserLoginSharedPreferences(NavigationTester.this);
+        startActivity(i);
+    }
+    public void bookings(){
+        //Intent aboutIntent = new Intent(this, ProductListActivity.class);
+        Intent bookings = new Intent(NavigationTester.this, DeveloperPage.class);
+        startActivity(bookings);
+    }
+}
